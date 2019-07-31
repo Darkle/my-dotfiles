5f6f62956622a5a2b10cedcf0418e6d8ebf6fd75
Coop <Darkle@users.noreply.github.com>
2019-07-31 16:34:26 +1000

    Create .simple-eslintrc.js

---
+++ JS/.simple-eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: "module",
+    ecmaFeatures: {
+      globalReturn: true,
+      impliedStrict: true,
+      jsx: true
+    }
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  globals: {
+
+  },
+  env: {
+    node: true,
+    browser: true,
+    'shared-node-browser': true,
+    es6: true,
+    webextensions: true,
+    greasemonkey: true,
+  },
+  plugins: [
+  ],
+  rules: {
+    "no-unused-vars": ["error", { "varsIgnorePattern": "^React$" }]
+  }
+}
