f3cec98ae75018016a3834917fbdfb6cd9190b29
Coop <Darkle@users.noreply.github.com>
2019-11-24 13:24:36 +1100

    Update .simple-eslintrc.js

--- JS/.simple-eslintrc.js
+++ JS/.simple-eslintrc.js
@@ -16,8 +16,7 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    'shared-node-browser': true,
-    es6: true,
+    es2020: true,
     webextensions: true,
     greasemonkey: true,
   },
